414aeb16c5a2b16af08c65bf874ea108d506ce70
WilliamBarela <william.barela.dev@gmail.com>
2017-09-10 13:26:53 -0500

    included type check in setter for state

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-class Board:
+class Board(object):
     def __init__(self, board_size = 19):
         self.__board_size = board_size
         self.state = self.opening_state(board_size)
@@ -7,3 +7,12 @@ class Board:
         # replace "cell" with cell data structure:
         return [["cell" for j in range(board_size)] for i in range(board_size)]
 
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, new_state):
+        instanceOfCell = [j == "cell" for i in new_state for j in i]
+        if not instanceOfCell: instanceOfCell = [False] # checks if empty set
+        if all(instanceOfCell): self.__state = new_state
