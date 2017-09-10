06a649a6d78946a66bce7e2d57a244df3d326237
WilliamBarela <william.barela.dev@gmail.com>
2017-09-10 10:52:00 -0500

    added opening_state method

--- board.py
+++ board.py
@@ -1,3 +1,9 @@
 class Board:
     def __init__(self, board_size = 19):
         self.__board_size = board_size
+        self.state = opening_state(board_size)
+
+    def opening_state(board_size):
+        # replace "cell" with cell data structure:
+        return [["cell" for j in range(board_size)] for i in range(board_size)]
+
