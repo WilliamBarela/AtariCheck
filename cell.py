713f4bb385446eee01de93fe6640f7ff7a5325e9
WilliamBarela <william.barela.dev@gmail.com>
2017-09-10 19:37:17 -0500

    added color setter and getters and validations

--- cell.py
+++ cell.py
@@ -3,12 +3,21 @@ class Cell(object):
         self._board_size = board_size
         self.i = i
         self.j = j
-        # make setters and getters for i and j
+        self.color = None
+        self.__liberties = self.init_liberties()
         # add liberties
 
     def isOnBoard(self, iterator):
         if not (iterator >= 0 and iterator < self._board_size):
             raise ValueError("i or j coordinate is invalid.")
+        else:
+            return True
+
+    def isValidColor(self, color):
+        valid_colors = [None, "black", "white"]
+
+        if color not in valid_colors:
+            raise ValueError("Color invalid: (choose: None, black, or white)")
         else:
             return True
 
@@ -28,3 +37,17 @@ class Cell(object):
     def j(self, j):
         if self.isOnBoard(j): self.__j = j
 
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, color):
+        if self.isValidColor(color): self.__color = color
+
+    @property
+    def liberties(self):
+        return self.__liberties
+
+    def init_liberties(self):
+        return {"north"}
