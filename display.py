a2ad73a28d6efdfd05d6aefc35f1a93889535f57
WilliamBarela <william.barela.dev@gmail.com>
2017-09-12 02:19:31 -0500

    changed display.py Added display method which displays the current state in the CLI

--- display.py
+++ display.py
@@ -4,3 +4,14 @@ from cell import Cell
 class Display(Board):
     def __init__(self):
         Board.__init__(self)
+
+    def board(self):
+        m_dash = "---"
+        divider = "---|" + "|".join([m_dash for cell in range(self.board_size)])
+        
+        print "   |" + "|".join(["%3s" % (i) for i in range(self.board_size)])
+        print divider
+        for i, row in enumerate(self.state):
+            row_num_formatted = "%2s" % (i) 
+            print row_num_formatted + " |" + " |".join([" " + cell.display for cell in row]) + " |"
+            print divider 
